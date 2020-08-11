8920a79c91b412c025cfc0fe330729ad8c66c666
James Grieve <jamesonrgrieve@gmail.com>
2020-08-11 14:47:42 -0600

    Two ifs, no else;

--- Program.cs
+++ Program.cs
@@ -6,56 +6,21 @@ namespace FirstDemo
     {
         static void Main(string[] args)
         {
-            /*
-                Title: Decisions Problem #3
+            int age;
 
-                Purpose: Determine whether a number entered by the user is a perfect square of a 
-                whole number or not.
+            Console.Write("Please enter a your age: ");
+            age = int.Parse(Console.ReadLine());
 
-                Author: James Grieve
-                Date of Last Edit: August 11, 2020
-            */
-
-            int menuChoice;
-
-            Console.WriteLine("Menu\n----------\n1. Option 1\n2. Option 2\n3. Option 3");
-            Console.Write("Please enter a menu choice: ");
-            menuChoice = int.Parse(Console.ReadLine());
-
-            // Menu with a switch.
-            switch (menuChoice)
+            if (age > 5)
             {
-                case 1:
-                    // Do the option 1 stuff.
-                    break;
-                case 2:
-                    // Do the option 2 stuff.
-                    break;
-                case 3:
-                    // Do the option 3 stuff.
-                    break;
-                default:
-                    // Invalid menu choice.
-                    break;
+                Console.WriteLine("You are older than five years old.");
+            }
+            // Without else, the second check is run. With else, only if the first check is false.
+            if (age > 10)
+            {
+                Console.WriteLine("You are older than ten years old.");
             }
             
-            // Menu with a if/else
-            if (menuChoice == 1)
-            {
-                // Do the option 1 stuff.
-            }
-            else if (menuChoice == 2)
-            {
-                // Do the option 2 stuff.
-            }
-            else if (menuChoice == 3)
-            {
-                // Do the option 1 stuff.
-            }
-            else
-            {
-                // Invalid menu choice.
-            }
 
         }
     }
