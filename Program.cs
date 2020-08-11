388fe15d51171234833eabca39ec901edda1dd14
James Grieve <jamesonrgrieve@gmail.com>
2020-08-11 09:52:55 -0600

    Inital commit;

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name; // "Declare" (create) a variable using the datatype followed by the name.
+
+            name = "James"; // Assign a value to the variable.
+
+            Console.WriteLine(name); // Retrieve the value from the variable and send it to the console.
+
+        }
+    }
+}
