83f01e6e56f6fb886f3e6b4338a40ef4476efd26
James Grieve <jamesonrgrieve@gmail.com>
2020-08-11 10:21:48 -0600

    Added data types;

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@ namespace FirstDemo
     {
         static void Main(string[] args)
         {
-
-            string name; // "Declare" (create) a variable using the datatype followed by the name.
+            // "Declare" (create) a variable using the datatype followed by the name.
+            string name; // Strings store text.
+            int wholeNumber; // Int stores a number without a decimal point (positive or negative).
+            double number; // Double stores a number with a decimal point.
 
             Console.WriteLine("Please enter your name below:"); // Tell the user what we're expecting.
             name = Console.ReadLine(); // Assign a value to the variable (that we get from the user).
