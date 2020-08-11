9d2a6362976c758ad2aee92b6e5242d91811717a
James Grieve <jamesonrgrieve@gmail.com>
2020-08-11 11:14:37 -0600

    Calculator example;

--- Program.cs
+++ Program.cs
@@ -6,30 +6,20 @@ namespace FirstDemo
     {
         static void Main(string[] args)
         {
-            // "Declare" (create) a variable using the datatype followed by the name.
-            string text; // Strings store text.
-            char character; // Stores a single character (including space, newline, etc).
-            int wholeNumber; // Int stores a number without a decimal point (positive or negative).
-            // byte, short and long are other integer types with varying size limits.
-            double number; // Double stores a number with a decimal point.
-            // float and decimal are alternatives with quirks. Google them if you plan to use them.
-            bool boolean; // Bool stores a true or false value.
+            double leftOperand, rightOperand, result;
 
-            string name; // Declare our variable.
-            int age, birthYear;
+            Console.WriteLine("Please enter the left operand: ");
+            leftOperand = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Please enter your name below:"); // Tell the user what we're expecting.
-            name = Console.ReadLine(); // Assign a value to the variable (that we get from the user).
+            Console.WriteLine("Please enter the right operand: ");
+            rightOperand = int.Parse(Console.ReadLine());
+            
+            result = leftOperand * rightOperand;
 
-            Console.WriteLine("Please enter your age below:"); // Tell the user what we're expecting.
-            age = int.Parse(Console.ReadLine()); // Convert the user's input to an integer and store it.
-            // If the user enters something that isn't an integer, IT WILL BLOW UP.
+            Console.WriteLine("The result of "+leftOperand+" * "+rightOperand+ " is: "+result);
 
-            birthYear = DateTime.Now.Year - age;
-
-            Console.WriteLine("Hello, " + name + ". You were born in the year " + birthYear + ", assuming your birthday this year has passed."); // Retrieve the values from the variables and send them to the console.
-
-
+            // Remember that dividing an integer by an integer will always produce an integer (no decimals).
+            
         }
     }
 }
