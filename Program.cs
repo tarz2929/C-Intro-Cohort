2e169916023ea06ac749c017c626cbcbc9fcff70
James Grieve <jamesonrgrieve@gmail.com>
2020-08-11 14:13:20 -0600

    Solved decisions question 3 as a group;

--- Program.cs
+++ Program.cs
@@ -6,13 +6,42 @@ namespace FirstDemo
     {
         static void Main(string[] args)
         {
-            int number;
+            /*
+                Title: Decisions Problem #3
+
+                Purpose: Determine whether a number entered by the user is a perfect square of a 
+                whole number or not.
+
+                Author: James Grieve
+                Date of Last Edit: August 11, 2020
+            */
+
+            int userNumber;
 
             Console.Write("Please enter a number: ");
-            number = int.Parse(Console.ReadLine());
+            userNumber = int.Parse(Console.ReadLine());
 
-            // Ternary Expression
-            Console.WriteLine("The number "+number+" is "+ (number % 2 == 0 ? "even" : "odd") + ".");
+
+            if (Math.Sqrt(userNumber) % 1 == 0)
+            {
+                Console.WriteLine("That number is a perfect square.");
+            }
+            else 
+            {
+                Console.WriteLine("That number is not a perfect square.");
+            }
+
+
+
+            /*
+                Pseudo-code
+                -declare variables (input from user)
+                -get input from the user (number to check)
+                if the number is a perfect square 
+                    -output "number is perfect square"
+                otherwise
+                    -output "number is not perfect square"
+            */
 
 
         }
