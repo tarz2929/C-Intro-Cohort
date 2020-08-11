6e04cd7d1c92abbf648117cd3daa41ce859b55cb
James Grieve <jamesonrgrieve@gmail.com>
2020-08-11 14:58:47 -0600

    Sentinel value loop example;

--- Program.cs
+++ Program.cs
@@ -6,22 +6,28 @@ namespace FirstDemo
     {
         static void Main(string[] args)
         {
-            int age;
-
-            Console.Write("Please enter a your age: ");
-            age = int.Parse(Console.ReadLine());
-
-            if (age > 5)
-            {
-                Console.WriteLine("You are older than five years old.");
-            }
-            // Without else, the second check is run. With else, only if the first check is false.
-            if (age > 10)
-            {
-                Console.WriteLine("You are older than ten years old.");
-            }
+            int menuOption = -1;
             
+            while (menuOption != 0)
+            {
+                Console.WriteLine("Menu\n------\n1. Option 1\n2. Option 2\n3. Option 3\n4. Option 4\n0. Exit");
 
+                Console.Write("Please enter a menu option:");
+                menuOption = int.Parse(Console.ReadLine());
+
+                switch (menuOption)
+                {
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                        Console.WriteLine("Running option {0}.", menuOption);
+                        break;
+                    default:
+                        Console.WriteLine("Not a valid menu option.");
+                        break;
+                }
+            }
         }
     }
 }
