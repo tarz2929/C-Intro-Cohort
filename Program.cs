ee05a562833a2dc5edce52f3841af93fe4590f21
James Grieve <jamesonrgrieve@gmail.com>
2020-08-13 13:46:46 -0600

    Repo cleanup;

--- Program.cs
+++ Program.cs
@@ -6,29 +6,7 @@ namespace FirstDemo
     {
         static void Main(string[] args)
         {
-            // string myString = "Hello, world!";
-
-            // int counter = 0;
-            // while (counter < myString.Length) // Test step;
-            // {
-            //     Console.WriteLine($"String index {counter} is {myString[counter]}."); // Process step.
-            //     // counter = counter + 1; 
-            //     // counter += 1;
-            //     counter++; // Modify step.
-            // }
-            // Console.WriteLine("Done!");
-
-
-
-
-            string myString = "Hello, world!";
-
-            for (int i = myString.Length-1 /* Initialize */; i >= 0 /* Test */; i-- /* Modify */)
-            {
-                Console.WriteLine($"String index {i} is {myString[i]}.");
-            }
-            Console.WriteLine("Done!");
+            
         }
     }
 }
- 
