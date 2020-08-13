6958ee08ef4d352b57f939606ad2d8e2d487ddcc
James Grieve <jamesonrgrieve@gmail.com>
2020-08-13 14:08:37 -0600

    Added catch with exception message; Added multiple parameters;

--- Program.cs
+++ Program.cs
@@ -8,15 +8,15 @@ namespace FirstDemo
         {
             int age = -1, favourite = -1;
 
-            age = GetValidInt("Please enter your age: ");
+            age = GetValidInt("Please enter your age: ", 1, 100);
      
-            favourite = GetValidInt("Please enter your favourite number: ");
+            favourite = GetValidInt("Please enter your favourite number: ", 0, 1000);
 
             Console.WriteLine($"Your age is {age}, your favourite number is {favourite}.");
 
 
         }
-        static int GetValidInt(string prompt)
+        static int GetValidInt(string prompt, int min, int max)
         {
             bool valid = false;
             int myInt = -1;
@@ -27,11 +27,15 @@ namespace FirstDemo
                 try
                 {
                     myInt = int.Parse(Console.ReadLine());
+                    if (myInt < min || myInt > max)
+                    {
+                        throw new Exception("Provided integer was outside of the bounds specified.");
+                    }
                     valid = true;
                 }
-                catch 
+                catch (Exception ex)
                 {
-                    Console.WriteLine("Parse failed, please ensure you have entered an integer.");
+                    Console.WriteLine($"Parse failed: {ex.Message}");
                 }
             } while (!valid);
 
